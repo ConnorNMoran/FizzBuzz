a94cf2e1e54edb75d581bbfef1394d31086cbbb7
ConnorNMoran <connor.n.moran@gmail.com>
2018-12-06 16:46:15 +0000

    Commented Code
    Decided to make a seperate branch for the commented code. I wouldn't usually do this, however since the task is pretty simple and it isn't really needed I just put the comments on a side branch.

--- FizzBuzz/FizzBuzz/FizzBuzzProgram.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzProgram.cs
@@ -6,32 +6,37 @@ namespace FizzBuzz
     {
         private static void Main(string[] args)
         {
-            int number = 1;
-
-            while (number <= 100)
+            //Create for loop to increment number
+            for (int i = 1; i <= 100; i++)
             {
-                if (number % 3 == 0)
+                //Check if number is divisible by 3
+                if (i % 3 == 0)
                 {
+                    //Write "Fizz"
                     Console.Write("Fizz");
 
-                    if (number % 5 != 0)
+                    //Check if number is not divisible by 5
+                    if (i % 5 != 0)
                     {
+                        //Go to new line for next entry
                         Console.WriteLine();
                     }
                 }
 
-                if (number % 5 == 0)
+                //Check if number is divisible by 5, else check if number is not divisible by 3
+                if (i % 5 == 0)
                 {
+                    //Write "Buzz"
                     Console.WriteLine("Buzz");
                 }
-                else if (number % 3 != 0)
+                else if (i % 3 != 0)
                 {
-                    Console.WriteLine(number);
+                    //Write number
+                    Console.WriteLine(i);
                 }
-
-                number++;
             }
 
+            //Wait for user to enter key to be able to read console output
             Console.ReadKey();
         }
     }
