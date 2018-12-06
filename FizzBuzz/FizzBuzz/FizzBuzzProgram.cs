ce22f73eefd892f235b2e1fea69ea8da6655216d
ConnorNMoran <connor.n.moran@gmail.com>
2018-12-06 16:37:05 +0000

    Completed Program
    Finished task.

---
+++ FizzBuzz/FizzBuzz/FizzBuzzProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        private static void Main(string[] args)
+        {
+            int number = 1;
+
+            while (number <= 100)
+            {
+                if (number % 3 == 0)
+                {
+                    Console.Write("Fizz");
+
+                    if (number % 5 != 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+
+                if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 3 != 0)
+                {
+                    Console.WriteLine(number);
+                }
+
+                number++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
